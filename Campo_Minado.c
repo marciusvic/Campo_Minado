98985f25ba66ad363d7b1ed742191b072cacec67
marciusvic <marcio9769@gmail.com>
2022-07-14 23:36:13 -0300

    att

--- Campo_Minado.c
+++ Campo_Minado.c
@@ -1,15 +1,19 @@
+// CAMPO MINADO DO HIKIN E DO MARCIN;
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
 
+
 struct campo{
     int bomba;
     int numero;
     int revelado;
-    };
-    // se a posição for numero == 0 && bomba == 0, essa posição está vazia
-    
+};
+// se a posição for numero == 0 && bomba == 0, esta posição está vazia, portanto, deve-se realizar a chamada recursiva.
+
+
 int verifica_quantidade_de_bombas(int lin, int col, struct campo campo_minado[lin][col]){
     int x = 0;
     for(int i = 0; i < lin; i++){
@@ -27,30 +31,34 @@ int verifica_quantidade_de_bombas(int lin, int col, struct campo campo_minado[li
     }
 } // função para verificar se realmente há 40 bombas no campo, pois um laço (na função criar_campo_minado) com o condicional "i <= 40" pode gerar menos de 40//
 
-void criar_campo_minado(int lin, int col, struct campo campo_minado[lin][col]){
-    srand((unsigned)time(NULL)); // semente aleatoria
+
+void criar_campo(int lin, int col, struct campo campo_minado[lin][col]){
+    srand((unsigned)time(NULL)); // gera uma semente aleatoria
     for(int i = 0; i < lin; i++){
         for(int j = 0; j < col; j++){
             campo_minado[i][j].bomba = 0;
             campo_minado[i][j].numero = 0;
             campo_minado[i][j].revelado = 0;
         }
+        // Serve para atribuir 0 em todas as posições do campo.
     }
-    int lin_bomb, col_bomb;
-    for (int i = 0; verifica_quantidade_de_bombas(lin, col, campo_minado) != 1; i++){
-            lin_bomb = rand()%10; // gera numero aleatorio de 0 a 9
-            col_bomb = rand()%20;// gera numero aleatorio de 0 a 19
-            campo_minado[lin_bomb][col_bomb].bomba = 1;
-        }
-    } // essa função irá montar o campo minado, com todas as posições de bombas ja determinadas
 
-void gerar_casas_numeros(int lin, int col, struct campo campo_minado[lin][col]){
+    int lin_bomb, col_bomb;  // Buffer que auxilía no armazenamento das posições das bombas.
+    while(verifica_quantidade_de_bombas(lin, col, campo_minado) != 1){          // {Alteração} (1)
+        lin_bomb = rand()%10; // gera numero aleatorio de 0 a 9
+        col_bomb = rand()%20;// gera numero aleatorio de 0 a 19
+        campo_minado[lin_bomb][col_bomb].bomba = 1;
+    }
+} // função que reseta o campo minado e define as posições das bombas.
+
+
+void gera_numeros(int lin, int col, struct campo campo_minado[lin][col]){
     for(int i = 0; i < lin; i++){
         for(int j = 0; j < col; j++){
             if(campo_minado[i][j].bomba == 1){
                 for(int k = -1; k < 2; k++){
                     for(int n = -1; n < 2; n++){
-                        if(((i+k) >= 0 && (j+n) >= 0)  && ((i+k) <lin && (j+n) <col)){
+                        if(((i+k) >= 0 && (j+n) >= 0)  && ((i+k) < lin && (j+n) < col)){
                             campo_minado[i+k][j+n].numero++;
                         }
                     }
@@ -60,54 +68,87 @@ void gerar_casas_numeros(int lin, int col, struct campo campo_minado[lin][col]){
     }
 }
 
-void print_mina(int lin, int col, struct campo campo_minado[lin][col]){
+/*int escolheu_bomba(int lin, int col, struct campo campo_minado[lin][col], ){
+
+}*/
+
+void formata_campo(int lin, int col, struct campo campo_minado[lin][col]){          // {NEW} (1)
     char c = 'a';
-    printf("   ");
+    char caractere = '+';
+    printf("     ");
     for(int i = 0; i < col; i++){
-        printf ("%c ", c++);
+        printf ("%c     ", c++);
     }
     printf("\n");
     for(int i = 0; i < lin; i++){
-        printf("%02d ", i+1);
+        printf("%.2d ", i+1);
         for(int j = 0; j < col; j++){
             if(campo_minado[i][j].revelado == 0){
-                printf("* ");
+                caractere = '+';
+                printf("| %c | ", caractere);
+            }// printar casas nao reveladas
+            else if(campo_minado[i][j].revelado == 1 && campo_minado[i][j].numero == 0 && campo_minado[i][j].bomba == 0){
+                caractere = ' ';
+                printf("| %c | ", caractere);
+                // CHAMAR A FUNÇÃO RECURSIVA AQUI.
+            }//printar casar vazias
+            else if(campo_minado[i][j].revelado == 1 && campo_minado[i][j].bomba == 0 && campo_minado[i][j].numero > 0){
+                printf("| %d | ", campo_minado[i][j].numero);
+            }//printar numeros
+            else if(campo_minado[i][j].bomba == 1){
+                // EXIBIR A LOCALIZAÇÃO DE TODAS AS BOMBAS DO CAMPO, E FINALIZAR O JOGO.
             }
-            /*if(campo_minado[i][j].bomba == 1){
-                printf("o ");
-            }* fins de teste*/
         }
         printf("\n");
     }
-    //essa função ira imprimir o campo minado para o usuario
-    //ainda sera mudada as condições do if adcionando caso tenha marcações, bombas, ou numero
-    //implementar para caso campo_minado[][].bomba == 1 && campo_minado[][].revelado == 0 && campo_minado[][].numero > 0
-}
+} // Ordem de jogada: primeiro linha, depois coluna. Ex: 1 B / 7 R / 10 N.
 
- 
+
 
 int main(){
     
     int lin = 10, col = 20;
-    
     struct campo campo_minado[lin][col];
     
-    criar_campo_minado(lin, col, campo_minado);
-    
-    print_mina(lin, col, campo_minado);
-    
-    /*int x = 0;
-    for(int i = 0; i < lin; i++){
-        for(int j = 0; j < col; j++){
-            if(campo_minado[i][j].bomba == 1){
-                x++;
-            }
+    criar_campo(lin, col, campo_minado);
+    gera_numeros(lin, col, campo_minado);
+    int opcao = 0;
+    printf("Seja Bem-vindo ao Campo Minado\n");
+    while(opcao != 2){
+        printf("Digite o número das opções a seguir:\n");
+        printf("1.Tutorial\n");
+        printf("2.Começar o Jogo\n");
+        scanf("%d", &opcao);
+        if(opcao == 1){
+            printf("Tutorial: O campo do jogo iniciará com todas as casas estando não-reveladas, você deve revelar alguma casa, e acontecerá isso:\nse a posição escolhida for vazia, debloqueará em cascata várias posições vazias\nse a posição escolhida for um número n, isso significa que em volta daquela casa haverá n bombas\nse a opsição escolhida for uma bomba, você perde o jogo."); //turorial do jogo aqui
         }
     }
+    //formata_campo(lin, col, campo_minado);
     
-    printf("\n%d", x);*/
-    
+
     
     
     return 0;
 }
+
+/* Remeleixos do Hikinho:          data: (14/07/22)  hora: (12:00 - xx:xx)
+
+  -  "// {Alteração} (x)" será o indicador de que alí houve uma alteração, seguido de seu número, para facilitar a verificação dos detalhes.
+
+ (1)    {Alteração}  ->  Substituí o for por um while pra ficar mais intuitívo.     (linha 47)
+
+
+ ~ N O V I D A D E S:
+
+ (1)    {new}  ->  reformulei e complementei a função de formatar o campo.     (linha 66)
+
+ -=+{DETALHES}+=-
+ - Nova formatação para o campo
+ - implementação do caso número.
+ - implementação da recursividade. (em stand-by)(falta fazer a função recursiva)
+ - implementação da exibição de todas as bombas do campo, caso o usuário perca. (em stand-by) (falta armazenar as posições das bombas)
+
+
+ (2)    {new}  ->  reformulei a função que define os números do campo. tinha problemas na função original     (linha 55) 
+
+*/
